4823aec15e2550966e21d6400dbbea785b128531
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-21 01:56:08 +0500

    update Zadacha_38

--- Zadacha_38/Program.cs
+++ Zadacha_38/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Microsoft.CodeAnalysis.CSharp.Scripting;
-
-// Задача 38: Задайте массив вещественных чисел.
+﻿// Задача 38: Задайте массив вещественных чисел.
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
@@ -9,120 +6,49 @@ using Microsoft.CodeAnalysis.CSharp.Scripting;
 // Они могут быть положительными или отрицательными.
 // Примеры: 15, 56.22, 0.0, -232336.11
 
-// enum Comparison_Operators
-// {
-//     =
-//     >
-//     >=
-//     <
-//     <=
-// }
 
+// int count = 25;
+int arrayLenght = 10;
+int rndDoubleLenght = 101;
+// double rndDoubleLenghtMin = -rndDoubleLenghtMax;
 
+double[] arrayDoubleList = new double[arrayLenght];
 
-// double ForDouble(string AssignmentToAVariableI, string ILessThanLength, string ActionWithVariableI) // for to while eval or exec
-// {
-//     string i = CSharpScript.EvaluateAsync(AssignmentToAVariableI);
-    
-//     return 0;
-// }
-
-
-
-
-
-
-
-
-//     for      (int    i = 0; i      <                           length;        i++)
-double ForDouble(double i, string ComparisonOperators, double lenght, string script)
+for (int i = 0; i < arrayLenght; i++)
 {
-    double firstNum = i;
-    double lenghtNum = lenght;
-    string strComprOprtr = ComparisonOperators;
-    string scriptFirst = script;
-    bool CompOper = true;
-    while(CompOper)
-    {
-        switch (strComprOprtr)
-        {
-        case "==":
-            CompOper = (firstNum == lenghtNum);
-            break;
-
-        case ">":
-            CompOper = (firstNum > lenghtNum);
-            break;
-
-        case ">=":
-            CompOper = (firstNum >= lenghtNum);
-            break;
-
-        case "<":
-            CompOper = (firstNum < lenghtNum);
-            break;
-
-        case "<=":
-            CompOper = (firstNum <= lenghtNum);
-            break;
-        default:
-            break;
-        }
-        CompOper = false;
-    }
-    CSharpScript.EvaluateAsync(scriptFirst);
-
-
-    Console.WriteLine(firstNum);
-    return firstNum;
+    arrayDoubleList[i] = new Random().Next(0, rndDoubleLenght);
 }
 
 
 
+for (int i = 0; i < arrayDoubleList.Length; i++)
+{
+    Console.Write($"{arrayDoubleList[i]} ");
+}
+Console.WriteLine();
 
 
-ForDouble(0, "<=", 1, "firstNum++");
+double[] minIndex = new double[] {arrayDoubleList[0], 0};
+double[] maxIndex = new double[] {arrayDoubleList[0], 0};
+for (int i = 1; i < arrayDoubleList.Length; i++)
+{
+    if(minIndex[0] > arrayDoubleList[i])
+    {
+        minIndex[0] = arrayDoubleList[i];
+        minIndex[1] = i;
+    }
+    else if(maxIndex[0] < arrayDoubleList[i])
+    {
+        maxIndex[0] = arrayDoubleList[i];
+        maxIndex[1] = i;
+    }
+}
 
-// double[] ArrayGeneratorNumber(double count, double min, double max)
-// {
-//     double[] arrayList = new double[count];
-//     for (double i = 0; i < count; i++)
-//        arrayList[i] = new Random().Next(min, max);
-//     return arrayList;
-// }
+Console.WriteLine($"minValue: {minIndex[0]}, maxValue: {maxIndex[0]}\n" 
+                 +$"minIndex: {minIndex[1]}, maxIndex: {maxIndex[1]}");
 
-// double[] arrayList = ArrayGeneratorNumber(10, -100, 101);
+double theDifferenceBetweenTheMaximumAndMinimumInTheValue = Math.Abs(maxIndex[0] - minIndex[0]);
+int theDifferenceBetweenTheMaximumAndMinimumInTheIndex = Math.Abs(Convert.ToInt32(maxIndex[1]) - Convert.ToInt32(minIndex[1]));
 
-// double countSumChetnihChisel = 0;
-// double tempNumberI = 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (double i = 0; i < arrayList.Length; i++)
-//     if(i % 2 == 1) countSumChetnihChisel += arrayList[i];
-        
-
-// Console.Write("[");
-// for (double n = 0; n < arrayList.Length; n++)
-// {
-//     if(n < arrayList.Length - 1)
-//         Console.Write($"{arrayList[n]}, ");
-//     else
-//         Console.Write($"{arrayList[n]}");
-// }
-// Console.WriteLine($"] -> Сумма чисел на нечётных позициях: {countSumChetnihChisel}");
+Console.WriteLine($"Разница между Min и Max в значение: {theDifferenceBetweenTheMaximumAndMinimumInTheValue}\n"
+                 +$"Растояние между Min и Max в индексе: {theDifferenceBetweenTheMaximumAndMinimumInTheIndex}");
