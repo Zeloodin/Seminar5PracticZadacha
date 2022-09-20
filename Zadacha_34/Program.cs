317e8777c4e7b0b42fa1d398771ffd7582ae4d03
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-21 00:23:30 +0500

    add Zadacha_34

---
+++ Zadacha_34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными
+// трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] ArrayGeneratorNumber(int count, int min, int max)
+{
+    int[] arrayList = new int[count];
+    for (int i = 0; i < count; i++)
+       arrayList[i] = new Random().Next(min, max);
+    return arrayList;
+}
+
+int[] arrayList = ArrayGeneratorNumber(34, 100, 1000);
+
+int countChetnihChisel = 0;
+for (int i = 0; i < arrayList.Length; i++)
+    if(arrayList[i] % 2 == 0)
+        countChetnihChisel++;
+
+Console.Write("[");
+for (int n = 0; n < arrayList.Length; n++)
+{
+    if(n < arrayList.Length - 1)
+        Console.Write($"{arrayList[n]}, ");
+    else
+        Console.Write($"{arrayList[n]}");
+}
+Console.WriteLine($"] -> Колличество чётных чисел: {countChetnihChisel}");
